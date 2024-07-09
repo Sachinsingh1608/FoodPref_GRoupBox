7522ad1b9dbe4d1e25161553cc42a10d6066a13e
Sachinsingh1608 <sk379403@gmail.com>
2024-07-09 09:20:21 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,408 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Veg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Car = new System.Windows.Forms.CheckBox();
+            this.Bike = new System.Windows.Forms.CheckBox();
+            this.Cycle = new System.Windows.Forms.CheckBox();
+            this.Hindi = new System.Windows.Forms.RadioButton();
+            this.English = new System.Windows.Forms.RadioButton();
+            this.Urdu = new System.Windows.Forms.RadioButton();
+            this.Food = new System.Windows.Forms.GroupBox();
+            this.omnivores = new System.Windows.Forms.RadioButton();
+            this.Nonveg = new System.Windows.Forms.RadioButton();
+            this.VegBut = new System.Windows.Forms.RadioButton();
+            this.Gender = new System.Windows.Forms.GroupBox();
+            this.TransGender = new System.Windows.Forms.RadioButton();
+            this.Female = new System.Windows.Forms.RadioButton();
+            this.Male = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Data = new System.Windows.Forms.Label();
+            this.Name = new System.Windows.Forms.TextBox();
+            this.RollNoBut = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.DOB = new System.Windows.Forms.DateTimePicker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Food.SuspendLayout();
+            this.Gender.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(837, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "RollNo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(88, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Owns";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(626, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mother Tounge";
+            // 
+            // Car
+            // 
+            this.Car.AutoSize = true;
+            this.Car.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Car.Location = new System.Drawing.Point(84, 277);
+            this.Car.Name = "Car";
+            this.Car.Size = new System.Drawing.Size(70, 24);
+            this.Car.TabIndex = 4;
+            this.Car.Text = "Car";
+            this.Car.UseVisualStyleBackColor = true;
+            // 
+            // Bike
+            // 
+            this.Bike.AutoSize = true;
+            this.Bike.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bike.Location = new System.Drawing.Point(84, 318);
+            this.Bike.Name = "Bike";
+            this.Bike.Size = new System.Drawing.Size(83, 24);
+            this.Bike.TabIndex = 5;
+            this.Bike.Text = "Bike";
+            this.Bike.UseVisualStyleBackColor = true;
+            // 
+            // Cycle
+            // 
+            this.Cycle.AutoSize = true;
+            this.Cycle.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cycle.Location = new System.Drawing.Point(84, 369);
+            this.Cycle.Name = "Cycle";
+            this.Cycle.Size = new System.Drawing.Size(96, 24);
+            this.Cycle.TabIndex = 6;
+            this.Cycle.Text = "Cycle";
+            this.Cycle.UseVisualStyleBackColor = true;
+            // 
+            // Hindi
+            // 
+            this.Hindi.AutoSize = true;
+            this.Hindi.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hindi.Location = new System.Drawing.Point(629, 277);
+            this.Hindi.Name = "Hindi";
+            this.Hindi.Size = new System.Drawing.Size(95, 24);
+            this.Hindi.TabIndex = 7;
+            this.Hindi.TabStop = true;
+            this.Hindi.Text = "Hindi";
+            this.Hindi.UseVisualStyleBackColor = true;
+            // 
+            // English
+            // 
+            this.English.AutoSize = true;
+            this.English.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.English.Location = new System.Drawing.Point(629, 318);
+            this.English.Name = "English";
+            this.English.Size = new System.Drawing.Size(121, 24);
+            this.English.TabIndex = 8;
+            this.English.TabStop = true;
+            this.English.Text = "English";
+            this.English.UseVisualStyleBackColor = true;
+            // 
+            // Urdu
+            // 
+            this.Urdu.AutoSize = true;
+            this.Urdu.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Urdu.Location = new System.Drawing.Point(629, 368);
+            this.Urdu.Name = "Urdu";
+            this.Urdu.Size = new System.Drawing.Size(82, 24);
+            this.Urdu.TabIndex = 9;
+            this.Urdu.TabStop = true;
+            this.Urdu.Text = "Urdu";
+            this.Urdu.UseVisualStyleBackColor = true;
+            // 
+            // Food
+            // 
+            this.Food.Controls.Add(this.omnivores);
+            this.Food.Controls.Add(this.Nonveg);
+            this.Food.Controls.Add(this.VegBut);
+            this.Food.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Food.Location = new System.Drawing.Point(201, 227);
+            this.Food.Name = "Food";
+            this.Food.Size = new System.Drawing.Size(376, 190);
+            this.Food.TabIndex = 10;
+            this.Food.TabStop = false;
+            this.Food.Text = "Food Preferences";
+            this.Food.Enter += new System.EventHandler(this.Food_Enter);
+            // 
+            // omnivores
+            // 
+            this.omnivores.AutoSize = true;
+            this.omnivores.Location = new System.Drawing.Point(24, 142);
+            this.omnivores.Name = "omnivores";
+            this.omnivores.Size = new System.Drawing.Size(147, 24);
+            this.omnivores.TabIndex = 13;
+            this.omnivores.TabStop = true;
+            this.omnivores.Text = "omnivores";
+            this.omnivores.UseVisualStyleBackColor = true;
+            // 
+            // Nonveg
+            // 
+            this.Nonveg.AutoSize = true;
+            this.Nonveg.Location = new System.Drawing.Point(24, 91);
+            this.Nonveg.Name = "Nonveg";
+            this.Nonveg.Size = new System.Drawing.Size(108, 24);
+            this.Nonveg.TabIndex = 12;
+            this.Nonveg.TabStop = true;
+            this.Nonveg.Text = "NonVeg";
+            this.Nonveg.UseVisualStyleBackColor = true;
+            // 
+            // VegBut
+            // 
+            this.VegBut.AutoSize = true;
+            this.VegBut.Location = new System.Drawing.Point(24, 50);
+            this.VegBut.Name = "VegBut";
+            this.VegBut.Size = new System.Drawing.Size(69, 24);
+            this.VegBut.TabIndex = 11;
+            this.VegBut.TabStop = true;
+            this.VegBut.Text = "Veg";
+            this.VegBut.UseVisualStyleBackColor = true;
+            // 
+            // Gender
+            // 
+            this.Gender.Controls.Add(this.TransGender);
+            this.Gender.Controls.Add(this.Female);
+            this.Gender.Controls.Add(this.Male);
+            this.Gender.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gender.Location = new System.Drawing.Point(875, 227);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(350, 198);
+            this.Gender.TabIndex = 11;
+            this.Gender.TabStop = false;
+            this.Gender.Text = "Gender";
+            this.Gender.Enter += new System.EventHandler(this.Gender_Enter);
+            // 
+            // TransGender
+            // 
+            this.TransGender.AutoSize = true;
+            this.TransGender.Location = new System.Drawing.Point(35, 135);
+            this.TransGender.Name = "TransGender";
+            this.TransGender.Size = new System.Drawing.Size(173, 24);
+            this.TransGender.TabIndex = 16;
+            this.TransGender.TabStop = true;
+            this.TransGender.Text = "TransGender";
+            this.TransGender.UseVisualStyleBackColor = true;
+            // 
+            // Female
+            // 
+            this.Female.AutoSize = true;
+            this.Female.Location = new System.Drawing.Point(35, 84);
+            this.Female.Name = "Female";
+            this.Female.Size = new System.Drawing.Size(108, 24);
+            this.Female.TabIndex = 15;
+            this.Female.TabStop = true;
+            this.Female.Text = "Female";
+            this.Female.UseVisualStyleBackColor = true;
+            // 
+            // Male
+            // 
+            this.Male.AutoSize = true;
+            this.Male.Location = new System.Drawing.Point(35, 43);
+            this.Male.Name = "Male";
+            this.Male.Size = new System.Drawing.Size(82, 24);
+            this.Male.TabIndex = 14;
+            this.Male.TabStop = true;
+            this.Male.Text = "Male";
+            this.Male.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(88, 469);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Shoes";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "One",
+            "Two",
+            "Three"});
+            this.comboBox1.Location = new System.Drawing.Point(90, 506);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(202, 28);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // Data
+            // 
+            this.Data.AutoSize = true;
+            this.Data.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Data.Location = new System.Drawing.Point(518, 514);
+            this.Data.Name = "Data";
+            this.Data.Size = new System.Drawing.Size(0, 20);
+            this.Data.TabIndex = 14;
+            // 
+            // Name
+            // 
+            this.Name.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name.Location = new System.Drawing.Point(203, 54);
+            this.Name.Multiline = true;
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(328, 36);
+            this.Name.TabIndex = 15;
+            // 
+            // RollNoBut
+            // 
+            this.RollNoBut.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RollNoBut.FormattingEnabled = true;
+            this.RollNoBut.Items.AddRange(new object[] {
+            "2022-01",
+            "2022-02",
+            "2022-03"});
+            this.RollNoBut.Location = new System.Drawing.Point(980, 49);
+            this.RollNoBut.Name = "RollNoBut";
+            this.RollNoBut.Size = new System.Drawing.Size(233, 28);
+            this.RollNoBut.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(81, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "DOB";
+            // 
+            // DOB
+            // 
+            this.DOB.CalendarFont = new System.Drawing.Font("Lucida Console", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DOB.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DOB.Location = new System.Drawing.Point(203, 115);
+            this.DOB.Name = "DOB";
+            this.DOB.Size = new System.Drawing.Size(430, 27);
+            this.DOB.TabIndex = 18;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Veg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1447, 708);
+            this.Controls.Add(this.DOB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.RollNoBut);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.Data);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Gender);
+            this.Controls.Add(this.Food);
+            this.Controls.Add(this.Urdu);
+            this.Controls.Add(this.English);
+            this.Controls.Add(this.Hindi);
+            this.Controls.Add(this.Cycle);
+            this.Controls.Add(this.Bike);
+            this.Controls.Add(this.Car);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Veg_Load);
+            this.Food.ResumeLayout(false);
+            this.Food.PerformLayout();
+            this.Gender.ResumeLayout(false);
+            this.Gender.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox Car;
+        private System.Windows.Forms.CheckBox Bike;
+        private System.Windows.Forms.CheckBox Cycle;
+        private System.Windows.Forms.RadioButton Hindi;
+        private System.Windows.Forms.RadioButton English;
+        private System.Windows.Forms.RadioButton Urdu;
+        private System.Windows.Forms.GroupBox Food;
+        private System.Windows.Forms.RadioButton omnivores;
+        private System.Windows.Forms.RadioButton Nonveg;
+        private System.Windows.Forms.RadioButton VegBut;
+        private System.Windows.Forms.GroupBox Gender;
+        private System.Windows.Forms.RadioButton TransGender;
+        private System.Windows.Forms.RadioButton Female;
+        private System.Windows.Forms.RadioButton Male;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label Data;
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.ComboBox RollNoBut;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker DOB;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace WindowsFormsApp1
+{
+    public partial class Veg : Form
+    {
+        public Veg()
+        {
+            InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            timer1 = new Timer();
+            timer1.Interval = 200; 
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Start();
+        }
+        private void Gender_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void LabelUpdate()
+        {
+            int lnCounter = 0;
+            string lsOwns = "";
+            if (Name.Text.Length > 0 && RollNoBut.Text.Length > 0 && DOB.Text.Length>0)
+            {
+                if (Car.Checked)
+                {
+                    lsOwns += 'Y';
+                }
+                else
+                {
+                    lsOwns += 'N';
+                }
+
+                if (Bike.Checked)
+                {
+                    lsOwns += 'Y';
+                }
+                else
+                {
+                    lsOwns += 'N';
+                }
+
+
+                if (Cycle.Checked)
+                {
+                    lsOwns += 'Y';
+                }
+                else
+                {
+                    lsOwns += 'N';
+                }
+                lnCounter++;
+            }
+            char lcFood='X' ;
+            if (lnCounter == 1)
+            {
+                
+               if(VegBut.Checked)
+                {
+                    lcFood = 'V';
+                    lnCounter++;
+                }
+               if(Nonveg.Checked)
+                {
+                    lcFood = 'N';
+                    lnCounter++;
+                }
+               if(omnivores.Checked)
+                {
+                    lcFood = 'O';
+                    lnCounter++;
+                }
+              
+
+            }
+
+            char lcMotherToung='X' ;
+          if(lnCounter == 2)
+            {
+               if(Hindi.Checked)
+                {
+                    lcMotherToung = 'H';
+                    lnCounter++;
+                }
+               if(English.Checked)
+                {
+                    lcMotherToung = 'E';
+                    lnCounter++;
+                }
+                if (Urdu.Checked)
+                {
+                    lcMotherToung = 'U';
+                    lnCounter++;
+                }
+               
+            }
+            char lcgender = 'X';
+          if(lnCounter == 3)
+            {
+                if(Male.Checked)
+                {
+                    lcgender = 'M';
+                    lnCounter++;
+                }
+                if(Female.Checked)
+                {
+                    lcgender = 'F';
+                    lnCounter++;
+                }
+                if(TransGender.Checked)
+                {
+                    lcgender = 'T';
+                    lnCounter++;
+                }
+           
+            }
+
+           
+
+            if (lnCounter == 4)
+            {
+                Data.Text = RollNoBut.Text + " , " + Name.Text + " , " + DOB.Text + " , " + lsOwns + " , " + lcFood + " , " + lcMotherToung+" , "+lcgender;
+            }
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            LabelUpdate();
+        }
+
+        private void Veg_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Food_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
